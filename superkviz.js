56fcd54c336bf2bdb668c7dee43b89678f7dd772
LAPTOP-5LG8T5MU\terka <terka.kop@seznam.cz>
2022-04-05 15:06:30 +0200

    Ukládá se hodnota do poleKviz pro skutečnou odpověď

--- superkviz.js
+++ superkviz.js
@@ -19,7 +19,11 @@ let poleKviz = [
 //poleKviz[0].poleMoznosti[cisloSpravneOdpovediPrvniOtazky]
 
 function pokracuj(event) {
-  console.log(event.srcElement.getAttribute('data-odpoved'))
+  poleKviz.forEach(element => {
+    element.skutecnaOdpoved = parseInt((event.srcElement.getAttribute('data-odpoved')))
+  });
+  console.log(poleKviz);
+
   if (cisloOtazky < poleKviz.length - 1) {
     kviz.innerHTML = ''
     cisloOtazky = cisloOtazky + 1
@@ -42,30 +46,31 @@ function napisOtazku() {
   poradi.textContent = 'OTÁZKA' + ' ' + (cisloOtazky + 1) + '/' + poleKviz.length;
   otazka.appendChild(poradi);
   //otazka.insertAdjacentElement(2, poradi);
-   
+
   const fotoKOtazce = document.createElement('img');
   fotoKOtazce.id = ('obrazek');
   fotoKOtazce.classList.add('foto');
   fotoKOtazce.src = poleKviz[cisloOtazky].foto;
   fotoKOtazce.alt = poleKviz[cisloOtazky].otazka;
   otazka.appendChild(fotoKOtazce);
-  
+
   const odpovedi = document.createElement('ul');
-  odpovedi.id = ('odpovedi'); 
+  odpovedi.id = ('odpovedi');
   otazka.appendChild(odpovedi);
 
   for (let i = 0; i < poleKviz[cisloOtazky].poleMoznosti.length; i++) {
     const moznosti = document.createElement('li');
     moznosti.id = ('moznosti');
-    
+
     moznosti.setAttribute('data-odpoved', i)
     moznosti.onclick = pokracuj;
 
     moznosti.innerHTML += poleKviz[cisloOtazky].poleMoznosti[i];
     odpovedi.appendChild(moznosti);
   }
-  
 }
+
+
 
 
 
