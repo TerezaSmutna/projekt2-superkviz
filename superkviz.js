b9797a40794d0d57eb6728560c5783895fb58bdb
LAPTOP-5LG8T5MU\terka <terka.kop@seznam.cz>
2022-04-07 20:33:37 +0200

    Vyhodnoceni not working in HTML

--- superkviz.js
+++ superkviz.js
@@ -13,7 +13,12 @@ let poleKviz = [
     otazka: 'Jaká budova nepatří mezi scény Národního divadla?', foto: 'obrazky/Národní divadlo.jpg',
     poleMoznosti: ['Stavovské divadlo', 'Státní opera Praha', 'Rudolfinum'],
     spravnaOdpoved: 2
-  }
+  },
+  {
+    otazka: 'V jakém roce byl založen Klub českých turistů?', foto: 'obrazky/klub českých turistů.jpg',
+    poleMoznosti: ['1878', '1888', '1877'],
+    spravnaOdpoved: 1
+  },
 ]
 
 //cisloSpravneOdpovediPrvniOtazky = poleKviz[0].spravnaOdpoved
@@ -21,56 +26,58 @@ let poleKviz = [
 
 function pokracuj(event) {
 
-let moznosti = event.target;
-let odpoved = moznosti.dataset.odpoved;
+  let moznosti = event.target;
+  let odpoved = moznosti.dataset.odpoved;
 
   poleKviz.forEach(element => {
     element.skutecnaOdpoved;
-});
+  });
 
-poleKviz[cisloOtazky].skutecnaOdpoved = parseInt(odpoved);
+  poleKviz[cisloOtazky].skutecnaOdpoved = parseInt(odpoved);
 
-if (cisloOtazky < poleKviz.length - 1) {
+  if (cisloOtazky < poleKviz.length - 1) {
     kviz.innerHTML = ''
     cisloOtazky = cisloOtazky + 1
     napisOtazku();
   } else {
+    //kviz.textContent = ''
     vyhodnot();
   }
 }
 
+let otazka = document.createElement('div');
+otazka.id = ('otazka');
 
 function napisOtazku() {
-  const otazka = document.createElement('div');
-  otazka.id = ('otazka');
+
   otazka.innerHTML = poleKviz[cisloOtazky].otazka
   kviz.appendChild(otazka);
   //kviz.insertAdjacentElement(2, otazka);
 
-  const poradi = document.createElement('div');
+  let poradi = document.createElement('div');
   poradi.id = ('poradi');
   poradi.textContent = 'OTÁZKA' + ' ' + (cisloOtazky + 1) + '/' + poleKviz.length;
-  otazka.appendChild(poradi); 
+  otazka.appendChild(poradi);
   //otazka.insertAdjacentElement(2, poradi);
 
-  const fotoKOtazce = document.createElement('img');
+  let fotoKOtazce = document.createElement('img');
   fotoKOtazce.id = ('obrazek');
   fotoKOtazce.classList.add('foto');
   fotoKOtazce.src = poleKviz[cisloOtazky].foto;
   fotoKOtazce.alt = poleKviz[cisloOtazky].otazka;
   otazka.appendChild(fotoKOtazce);
 
-  const odpovedi = document.createElement('ul');
+  let odpovedi = document.createElement('ul');
   odpovedi.id = ('odpovedi');
   otazka.appendChild(odpovedi);
 
   for (let i = 0; i < poleKviz[cisloOtazky].poleMoznosti.length; i++) {
-    const moznosti = document.createElement('li');
+    let moznosti = document.createElement('li');
     moznosti.id = ('moznosti');
 
     moznosti.setAttribute('data-odpoved', i);
     moznosti.addEventListener('click', pokracuj);
-     //moznosti.onclick = pokracuj;
+    //moznosti.onclick = pokracuj;
 
     moznosti.innerHTML += poleKviz[cisloOtazky].poleMoznosti[i];
     odpovedi.appendChild(moznosti);
@@ -78,13 +85,35 @@ function napisOtazku() {
 }
 
 function vyhodnot() {
-  kviz.innerHTML = ''
-  for (let i = 0; i < poleKviz.length; i++) {
-    const hodnoceniNadpis = document.createElement('div');
-    hodnoceniNadpis.classList.add('vysledek');
-    kviz.appendChild(hodnoceniNadpis);
-    hodnoceniNadpis.textContent = 'TVOJE HODNOCENÍ';
+  otazka.innerHTML = poleKviz[cisloOtazky].otazka
+  let skutecnaOdpoved = poleKviz.skutecnaOdpoved;
+  let spravnaOdpoved = poleKviz.spravnaOdpoved;
+
+  //for (let i = 0; i < poleKviz.length; i++) {
+  for (let i = 0; i < 1; i++) {
+    let tvojehodnoceni = document.querySelector('#tvojehodnoceni');
+    tvojehodnoceni.textContent = 'TVOJE HODNOCENÍ';
+    
+    let otazkaHodnoceni = document.querySelector('.vysledek');
+    otazkaHodnoceni.textContent = (i + 1) + '. ' + poleKviz[i].otazka;
+
+    let hodnoceni = document.createElement('div');
+
+    let dobraOdpoved = ('Správná odpověď: ' + spravnaOdpoved);
+
+    hodnoceni.id = ('hodnoceni');
+    hodnoceni.textContent = 'Tvoje odpověď: ' + poleKviz[i].skutecnaOdpoved + '<br>' + dobraOdpoved;
+
+    if (skutecnaOdpoved == spravnaOdpoved) {
+      dobraOdpoved = true;
+      dobraOdpoved.textContent = 'To je SPRÁVNĚ.'
+    }
   }
+
+  let procentoUspesnosti = document.createElement('div');
+  //procentoUspesnosti.id = ('');
+  procentoUspesnosti.textContent = 'SPRÁVNĚ' + 1 + 'ZE' + poleKviz.length + 'OTÁZEK. ÚSPĚŠNOST' + 33 + '%.'
+
 }
 
 
